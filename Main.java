1cd214c83b54f8dcac758ed5bd254709827bbb4b
saidasaduakas <80287291+saidasaduakas@users.noreply.github.com>
2021-09-21 22:06:09 +0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Plane plane=new Plane();
+
+        plane.display();
+        plane.performdrive();
+        plane.performvoice();
+    }
+}
